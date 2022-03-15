226fe292bc73b7b2d2b1faa9546b652ef0243660
Dombwe <49947441+Dombwe@users.noreply.github.com>
2022-03-15 11:15:20 +0000

    Revert "Revert "first commit""
    This reverts commit 3bd7ef546a66792861c24e5c791f85b14376dcbe.

---
+++ src/controller/administrator.js
@@ -0,0 +1,24 @@
+import {BASE_URL, HEADERS} from "../config/http";
+import axios from "axios";
+
+const route = '/administrators';
+
+export async function login(username, password)  {
+    if (username.length > 0 && password.length > 0) {
+
+        const url = BASE_URL+`${route}/loginWithUsername/${username}/${password}`;
+        const response = await axios.get(url);
+    
+        if (response.status === 200) {
+            localStorage.setItem('token', response.data['token']);
+            localStorage.setItem('name', response.data['administrator'].name);
+            localStorage.setItem('username', response.data['administrator'].username);
+            localStorage.setItem('contact', response.data['administrator'].contact);
+            localStorage.setItem('level', response.data['administrator'].level);
+            return true;     
+        }
+        return false;  
+    } else {
+        return false;
+    }
+}
