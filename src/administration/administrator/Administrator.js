df2d2cfa150e39b81b6b72604feb86c7913125a0
Dombwe <49947441+Dombwe@users.noreply.github.com>
2022-03-23 15:47:57 +0000

    Administrator update

--- src/administration/administrator/Administrator.js
+++ src/administration/administrator/Administrator.js
@@ -59,27 +59,6 @@ class Administrator extends React.Component {
                       <th key={4} scope="col" className={"text-center pl-0"}>
                         Level
                       </th>
-                      <th key={5} scope="col" className={"text-center pl-0"}>
-                        Debut E
-                      </th>
-                      <th key={6} scope="col" className={"text-center pl-0"}>
-                        Fin E
-                      </th>
-                      <th key={7} scope="col" className={"text-center pl-0"}>
-                        Debut P
-                      </th>
-                      <th key={8} scope="col" className={"text-center pl-0"}>
-                        Fin P
-                      </th>
-                      <th key={9} scope="col" className={"text-center pl-0"}>
-                        Categorie
-                      </th>
-                      <th key={10} scope="col" className={"text-center pl-0"}>
-                        Utilisateur
-                      </th>
-                      <th key={11} scope="col" className={"text-center pl-0"}>
-                        Status
-                      </th>
                       <th key={12} scope="col" className={"text-center pl-0"}>
                         Action
                       </th>
@@ -87,29 +66,21 @@ class Administrator extends React.Component {
                   </thead>
                   <tbody className="text-dark">
                     {
-                      administrators && administrators.map((event, index) => { 
-                        if (event.status === 'waiting') {
-                          return ( 
-                            <tr key={index++}>
-                              <td scope='row'>{index}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.name}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.organizer}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.limit_registration}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.location +'('+event.city+')'}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.starting_date.toString()}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.ending_date}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.publishing_start_date}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.publishing_end_date}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.category}</td>
-                              <td className={"pl-0 fw-normal text-center"}>{event.account_id}</td>
-                              <td className={"pl-0 text-warning fw-normal"}>{event.status}</td>
-                              <td className={"pl-0 fw-normal"}>
-                                <a href="#" style={{fontSize:"20px", marginRight:"15px"}}><i class="text-success fa fa-check-circle"></i></a>
-                                <a href="#" style={{fontSize:"20px"}}><i class="text-danger fa fa-times-circle"></i></a>
-                              </td>                          
-                            </tr>
-                          );
-                        }
+                      administrators && administrators.map((administrator, index) => { 
+                        return ( 
+                          <tr key={index++}>
+
+                            <td scope='row'>{index}</td>
+                            <td className={"pl-0 fw-normal text-center"}>{administrator.name}</td>
+                            <td className={"pl-0 fw-normal text-center"}>{administrator.username}</td>
+                            <td className={"pl-0 fw-normal text-center"}>{administrator.contact}</td>
+                            <td className={"pl-0 fw-normal text-center"}>{administrator.level}</td>
+                            <td className={"pl-0 fw-normal"}>
+                              <a href="#" style={{fontSize:"20px", marginRight:"15px"}}><i class="text-success fa fa-check-circle"></i></a>
+                              <a href="#" style={{fontSize:"20px"}}><i class="text-danger fa fa-times-circle"></i></a>
+                            </td>                          
+                          </tr>
+                        );
                       })
                     }
                   </tbody>
